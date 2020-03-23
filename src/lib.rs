55e65ecb831d623f56736762f12b76a42f3adc55
tavariscodes <tavarisjohnson@Tavariss-MacBook-Air.local>
2020-03-22 23:53:49 -0500

    Env Variables

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitvie: bool,
 }
 
 impl Config {
@@ -14,14 +16,22 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok( Config {query, filename} )
+        let case_sensitvie = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok( Config { query, filename, case_sensitvie} )
     }
  }
 
  pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
      let contents = fs::read_to_string(config.filename)?;
+
+     let results = if config.case_sensitvie {
+         search(&config.query, &contents)
+     } else {
+         search_case_insensitive(&config.query, &contents)
+     };
      
-     for line in search(&config.query, &contents) {
+     for line in results {
          println!("{}", line);
      }
 
@@ -41,22 +51,49 @@ impl Config {
      results
  }
 
+ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+     let query = query.to_lowercase();
+     let mut results = Vec::new();
+
+     for line in contents.lines() {
+         if line.to_lowercase().contains(&query) {
+             results.push(line);
+         }
+     }
+
+     results
+ }
+
  #[cfg(test)]
 mod tests {
     use super::*;
-
+    
     #[test]
-    fn one_result() {
+    fn case_sensitvie() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.
-        ";
-
+Duct tape.";
         assert_eq!(
             vec!["safe, fast, productive."],
             search(query, contents)
+        )
+    }
+
+    #[test]
+    fn case_insensitvie() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
         );
     }
 }
